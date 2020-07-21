97fb25cf38bb7bbe132333639050efba4285e3c3
Alex Stockwell <astockwell@gmail.com>
2020-07-20 17:13:46 -0700

    updated comment docs

--- user_update_status.go
+++ user_update_status.go
@@ -2,26 +2,26 @@ package zoom
 
 import "fmt"
 
-// UpdateUserStatusPath - v2 path for creating a user
+// UpdateUserStatusPath - v2 path for updating a user's status
 const UpdateUserStatusPath = "/users/%s/status"
 
-// UpdateUserStatusURLOptions are the options to create a user with
+// UpdateUserStatusURLOptions are the options to identify a user
 type UpdateUserStatusURLOptions struct {
 	EmailOrID string `url:"-"`
 }
 
-// UpdateUserStatusDataOptions are details about a user to create
+// UpdateUserStatusDataOptions are details about a user's status to update
 type UpdateUserStatusDataOptions struct {
 	Action UpdateUserStatusAction `json:"action"`
 }
 
-// UpdateUserStatus calls POST /users/{userId}/meetings
+// UpdateUserStatus calls PUT /users/{userId}/status
 func UpdateUserStatus(urlOpts UpdateUserStatusURLOptions, dataOpts UpdateUserStatusDataOptions) error {
 	return defaultClient.UpdateUserStatus(urlOpts, dataOpts)
 }
 
-// UpdateUserStatus calls POST /users
-// https://marketplace.zoom.us/docs/api-reference/zoom-api/users/usercreate
+// UpdateUserStatus calls PUT /users/{userId}/status
+// https://marketplace.zoom.us/docs/api-reference/zoom-api/users/userstatus
 func (c *Client) UpdateUserStatus(urlOpts UpdateUserStatusURLOptions, dataOpts UpdateUserStatusDataOptions) error {
 	return c.requestV2(requestV2Opts{
 		Method:         Put,
