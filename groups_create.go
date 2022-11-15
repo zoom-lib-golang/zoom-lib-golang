0aff4df2f4789d33fb434a8075792ba29f68e462
y-ytakahashi <tk.y_y.prive@icloud.com>
2022-11-15 13:46:39 +0900

    Add create groups endpoint

---
+++ groups_create.go
@@ -0,0 +1,24 @@
+package zoom
+
+const CreateGroupsPath = "/groups"
+
+// CreateGroupOption - options for creating a group
+type CreateGroupOption struct {
+	Name string `json:"name"`
+}
+
+// CreateGroup calls POST /groups/{groupId}/members
+func CreateGroup(opts CreateGroupOption) error {
+	return defaultClient.CreateGroup(opts)
+}
+
+// CreateGroup calls POST /groups/{groupId}/members
+// https://marketplace.zoom.us/docs/api-reference/zoom-api/groups/groupmemberscreate
+func (c *Client) CreateGroup(opts CreateGroupOption) error {
+	return c.requestV2(requestV2Opts{
+		Method:         Post,
+		Path:           CreateGroupsPath,
+		DataParameters: &opts,
+		HeadResponse:   false,
+	})
+}
