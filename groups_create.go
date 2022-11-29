6d0ecd0fc29c8b2ac05b1369be284469350ce376
y-ytakahashi <tk.y_y.prive@icloud.com>
2022-11-29 13:43:16 +0900

    update comment .

--- groups_create.go
+++ groups_create.go
@@ -1,6 +1,6 @@
 package zoom
 
-// CreateGroupsPath -v2 path for add groups 
+// CreateGroupsPath -v2 path for add groups
 const CreateGroupsPath = "/groups"
 
 // CreateGroupOption - options for creating a group
@@ -8,13 +8,13 @@ type CreateGroupOption struct {
 	Name string `json:"name"`
 }
 
-// CreateGroup calls POST /groups/{groupId}/members
+// CreateGroup calls POST /groups
 func CreateGroup(opts CreateGroupOption) error {
 	return defaultClient.CreateGroup(opts)
 }
 
-// CreateGroup calls POST /groups/{groupId}/members
-// https://marketplace.zoom.us/docs/api-reference/zoom-api/groups/groupmemberscreate
+// CreateGroup calls POST /groups
+// https://marketplace.zoom.us/docs/api-reference/zoom-api/methods/#operation/groupCreate
 func (c *Client) CreateGroup(opts CreateGroupOption) error {
 	return c.requestV2(requestV2Opts{
 		Method:         Post,
