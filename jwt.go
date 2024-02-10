d936af5172415242206cd5fda11be39d81a90a75
Pablo Carranza <pcarranza@gmail.com>
2024-02-10 18:04:00 +0100

    Add oauth2 server to server support

--- jwt.go
+++ jwt.go
@@ -1,8 +1,6 @@
 package zoom
 
 import (
-	"log"
-	"net/http"
 	"time"
 
 	"github.com/golang-jwt/jwt/v4"
@@ -17,25 +15,4 @@ func jwtToken(key string, secret string) (string, error) {
 	token.Header["alg"] = "HS256"
 	token.Header["typ"] = "JWT"
 	return token.SignedString([]byte(secret))
-}
-
-func (c *Client) addRequestAuth(req *http.Request, err error) (*http.Request, error) {
-	if err != nil {
-		return nil, err
-	}
-
-	// establish JWT token
-	ss, err := jwtToken(c.Key, c.Secret)
-	if err != nil {
-		return nil, err
-	}
-
-	if Debug {
-		log.Println("JWT Token: " + ss)
-	}
-
-	// set JWT Authorization header
-	req.Header.Add("Authorization", "Bearer "+ss)
-
-	return req, nil
 }
